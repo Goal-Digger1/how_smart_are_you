61aaa71ad444cecc1ff0e02f5ba3cda2604fed9b
Alexander <alexander.cubitchi@student.upt.ro>
2021-10-26 02:27:59 +0300

    DatabaseSetup
    initialized mysql db connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    body: const Center(
-      child: Text(
-        'How smart are you?',
-        style: TextStyle(
-            fontSize: 24.0,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'PlayfairDisplay'
+void main(
+    ) {
+  init();
+ runApp(MaterialApp(
+    home: Scaffold(
+      body: const Center(
+        child: Text(
+          'How smart are you?',
+          style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'PlayfairDisplay'
+          ),
         ),
       ),
     ),
-  ),
-));
+  ));
+}
+void init() async {
+  
+}
