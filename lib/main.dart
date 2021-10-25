bbbd57809ed67249c08664afb81d721baa4ad44d
Lorin <lorin.rapciuc@ligaac.ro>
2021-10-25 20:08:20 +0300

    Converted opening to stateless

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,37 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    body: const Center(
-      child: Text(
-        "\n\n\n\nHow smart are you?",
-        style: TextStyle(
-            fontSize: 24.0,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'PlayfairDisplay'
-        ),
-      ),
-    ),
-    floatingActionButton: Container(
-      height: 80.0,
-      width: 250.0,
-      child: FloatingActionButton(
-        onPressed: () {  },
-        child: const Text(
-          'Register',
-          //style:
-        ),
-        shape: BeveledRectangleBorder(),
-        backgroundColor: Colors.redAccent,
-      ),
-    ),
-    floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-  ),
+  home: Opening(),
 ));
+
+class Opening extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(
+        child: Text(
+          "\n\n\n\nHow smart are you?",
+          style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'PlayfairDisplay'
+          ),
+        ),
+      ),
+      floatingActionButton: Container(
+        height: 80.0,
+        width: 250.0,
+        child: FloatingActionButton(
+          onPressed: () {  },
+          child: const Text(
+            'Register',
+            //style:
+          ),
+          shape: BeveledRectangleBorder(),
+          backgroundColor: Colors.redAccent,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
