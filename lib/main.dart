63ceb0cc1cb3348a5672da6a67b55e68306e3c4f
Alexander <alexander.cubitchi@student.upt.ro>
2021-10-26 14:53:45 +0300

    Updated Dart and added dependencies for Hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:mysql_utils/mysql_utils.dart';
 
-void main(
-    ) {
-  init();
+ void main (
+    )  {
+  initDB();
  runApp(MaterialApp(
     home: Scaffold(
       body: const Center(
@@ -18,6 +19,30 @@ void main(
     ),
   ));
 }
-void init() async {
-  
-}
+void initDB() async {
+   final db = MysqlUtils(
+        settings: ConnectionSettings(
+          host: 'database-1.c5txbcqs9xrv.us-east-2.rds.amazonaws.com',
+          port: 3306,
+          user: 'admin',
+          password: '11223344',
+          db: 'howsmartareyou',
+          useCompression: false,
+          useSSL: false,
+          timeout: const Duration(seconds: 10),
+        ),
+        prefix: '',
+        pool: true,
+        errorLog: (error) {
+          print('|$error\n├───────────────────────────');
+        },
+        sqlLog: (sql) {
+          print('|$sql\n├───────────────────────────');
+        },
+        connectInit: (db1) async {
+          print('whenComplete');
+        });
+   //     var row = await db
+   //     .query('select * from Username where id=?', [1]);
+   // print(row);
+  }
