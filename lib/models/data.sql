e966b6a178b80e8268afa8ed82fc03767abde282
Alexander <alexander.cubitchi@student.upt.ro>
2021-10-29 18:26:31 +0300

    updated the question table

--- lib/models/data.sql
+++ lib/models/data.sql
@@ -5,7 +5,8 @@ CREATE TABLE `Category` (
                            PRIMARY KEY (`id`),
                        UNIQUE KEY `id_UNIQUE` (`id`)
                        ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
-CREATE TABLE `Level` (`id` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `Level` (
+                        `id` int NOT NULL AUTO_INCREMENT,
                         `category_id` int NOT NULL,
                         `xpRequirement` int NOT NULL,
                          PRIMARY KEY (`id`),
@@ -14,16 +15,19 @@ CREATE TABLE `Level` (`id` int NOT NULL AUTO_INCREMENT,
                          ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 
 CREATE TABLE `Question` (
-                             `id` int NOT NULL AUTO_INCREMENT,
-                             `level_id` int NOT NULL,
-                             `correct` varchar(50) NOT NULL,
-                             `wrong1` varchar(50) NOT NULL,
-                             `wrong2` varchar(50) NOT NULL,
-                             `wrong3` varchar(50) NOT NULL,
-                             PRIMARY KEY (`id`),
-                             UNIQUE KEY `id_UNIQUE` (`id`),
-                             CONSTRAINT `question_fk` FOREIGN KEY (`level_id`) REFERENCES `Level` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+  `id` int NOT NULL AUTO_INCREMENT,
+  `level_id` int NOT NULL,
+  `statement` varchar(255) NOT NULL,
+  `correct` varchar(50) NOT NULL,
+  `wrong1` varchar(50) NOT NULL,
+  `wrong2` varchar(50) NOT NULL,
+  `wrong3` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `question_fk` (`level_id`),
+  CONSTRAINT `question_fk` FOREIGN KEY (`level_id`) REFERENCES `Level` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 
 Create table `Data` (  `version` int NOT NULL  )
 
