68711f9ad67ecd2d02d1f716de84fae9a8c4f2b9
Alexander <alexander.cubitchi@student.upt.ro>
2021-11-14 15:54:48 +0200

    Added a user avatar and lvl bar

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -5,6 +5,8 @@ import 'package:how_smart_are_you/pages/home/categoryController.dart';
 import 'package:how_smart_are_you/pages/home/categoryTile.dart';
 import 'package:how_smart_are_you/services/HiveServices.dart';
 import 'package:how_smart_are_you/services/WidgetService.dart';
+import 'package:how_smart_are_you/services/ui.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -29,10 +31,56 @@ class _HomeState extends State<Home> {
           showTrackOnHover: true,
           child: SingleChildScrollView(
             child: Column(
-              children: HiveServices().getAllCategories()
-                .map((category) => CategoryController(cat: category,
-              )).toList(),
+              children: [
+                Container(
+                  height: 150,
+                  padding: EdgeInsets.symmetric(horizontal: AppMargins.General),
+                  child: Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 40,
+                        backgroundColor: Colors.deepOrangeAccent,
+                        child: Text(
+                          HiveServices().getUser().nickname,
+                          style: TextStyle(
+                            fontSize: AppFontSizes.XL,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                              padding: EdgeInsets.all(AppMargins.Secondary),
+                              child: LinearPercentIndicator(
+                                lineHeight: 25,
+                                width: MediaQuery.of(context).size.width-200,
+                                percent: ((HiveServices().getUser().exp % 100)/100),
+                                backgroundColor: AppColors.BlueLight,
+                                progressColor: Colors.tealAccent[400],
+                                center: Text(
+                                    "Level: "+(HiveServices().getUser().exp/100).floor().toString(),
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: AppFontSizes.S,
+                                    ),
+                                ),
+                              ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                Column(
+                  children: HiveServices().getAllCategories()
+                    .map((category) => CategoryController(cat: category,
+                  )).toList(),
           ),
+              ],
+            ),
 
     ),
         ),
